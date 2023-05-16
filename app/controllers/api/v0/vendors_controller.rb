24bb476081c4859feecc497c5a62b35a2ff8a21f
Reid Miller <reid.s.miller@gmail.com>
2023-05-16 16:55:33 -0600

    feat: Add vendor controller and serializer

---
+++ app/controllers/api/v0/vendors_controller.rb
@@ -0,0 +1,7 @@
+class Api::V0::VendorsController < ApplicationController
+  def index
+    market = Market.find(params[:market_id])
+    vendors = market.vendors
+    render json: VendorSerializer.new(vendors)
+  end
+end
